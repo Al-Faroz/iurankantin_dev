5baf359b34b0c761573c23e03b4eb5c8f249fbb2
Al-Faroz <151546815+Al-Faroz@users.noreply.github.com>
2026-09-13 10:02:08 +0700

    feat: add setoran pimpinan model

---
+++ app/Models/SetoranPimpinanModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class SetoranPimpinanModel extends Model
+{
+    protected $table = 'setoran_pimpinan';
+    protected $primaryKey = 'id_setoran';
+    protected $returnType = 'array';
+    protected $useAutoIncrement = true;
+    protected $protectFields = true;
+    protected $allowedFields = [
+        'tanggal_form',
+        'periode_awal',
+        'periode_akhir',
+        'nominal',
+        'keterangan',
+        'id_operator',
+        'created_at',
+    ];
+
+    protected $useTimestamps = false;
+}
