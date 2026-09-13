bc58e41afab5eea7b4c96dabcd1054394acc2cba
Al-Faroz <151546815+Al-Faroz@users.noreply.github.com>
2026-09-13 09:58:48 +0700

    feat: add operator routes for master data

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,28 @@ use CodeIgniter\Router\RouteCollection;
 /** @var RouteCollection $routes */
 $routes->get('/', 'Auth::login');
 $routes->get('login', 'Auth::login', ['as' => 'login']);
-$routes->post('login', 'Auth::attempt', ['filter' => 'csrf']);
-$routes->post('logout', 'Auth::logout', ['filter' => 'csrf']);
+$routes->post('login', 'Auth::attempt');
+$routes->post('logout', 'Auth::logout');
 
 $routes->get('dashboard', 'Dashboard::index', ['filter' => 'auth']);
+
+$routes->group('', ['filter' => 'operator'], static function (RouteCollection $routes): void {
+    $routes->get('golongan', 'GolonganPenjual::index');
+    $routes->get('golongan/tambah', 'GolonganPenjual::create');
+    $routes->post('golongan/simpan', 'GolonganPenjual::store');
+    $routes->get('golongan/(:num)/edit', 'GolonganPenjual::edit/$1');
+    $routes->post('golongan/(:num)/update', 'GolonganPenjual::update/$1');
+
+    $routes->get('kategori-pengeluaran', 'KategoriPengeluaran::index');
+    $routes->get('kategori-pengeluaran/tambah', 'KategoriPengeluaran::create');
+    $routes->post('kategori-pengeluaran/simpan', 'KategoriPengeluaran::store');
+    $routes->get('kategori-pengeluaran/(:num)/edit', 'KategoriPengeluaran::edit/$1');
+    $routes->post('kategori-pengeluaran/(:num)/update', 'KategoriPengeluaran::update/$1');
+
+    $routes->get('penjual', 'Penjual::index');
+    $routes->get('penjual/tambah', 'Penjual::create');
+    $routes->post('penjual/simpan', 'Penjual::store');
+    $routes->get('penjual/(:num)/edit', 'Penjual::edit/$1');
+    $routes->post('penjual/(:num)/update', 'Penjual::update/$1');
+    $routes->get('penjual/(:num)', 'Penjual::show/$1');
+});
