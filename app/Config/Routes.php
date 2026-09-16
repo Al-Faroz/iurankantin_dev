a92a0e508d528c985217f0a918ceea40969376e8
Al-Faroz <151546815+Al-Faroz@users.noreply.github.com>
2026-09-16 09:42:38 +0700

    security: add authenticated proof routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -46,12 +46,14 @@ $routes->group('', ['filter' => 'operator'], static function (RouteCollection $r
     $routes->get('pengeluaran', 'Pengeluaran::index');
     $routes->get('pengeluaran/tambah', 'Pengeluaran::create');
     $routes->post('pengeluaran/simpan', 'Pengeluaran::store');
+    $routes->get('pengeluaran/(:num)/bukti', 'Pengeluaran::bukti/$1');
 
     $routes->get('setoran', 'Setoran::index');
     $routes->get('setoran/cetak', 'Setoran::cetakForm');
     $routes->post('setoran/cetak-pdf', 'Setoran::cetakPdf');
     $routes->get('setoran/input', 'Setoran::input');
     $routes->post('setoran/simpan', 'Setoran::store');
+    $routes->get('setoran/(:num)/bukti', 'Setoran::bukti/$1');
     $routes->get('setoran/(:num)/cetak-ulang', 'Setoran::cetakUlang/$1');
     $routes->get('setoran/(:num)/edit', 'Setoran::edit/$1');
     $routes->post('setoran/(:num)/update', 'Setoran::update/$1');
